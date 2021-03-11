a18632bb2916ec4e2d84fa6b21b67a880182742a
Andrey007380 <Andrey.lishtva@gmail.com>
2021-03-11 17:08:12 +0200

    Стрельба
    стрельба только перед игроком

---
+++ Defence It/Assets/BulletMechanics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMechanics : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    void OnTriggerEnter(Collider co)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Defence It/Assets/Shouting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shouting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject BulletPrefab;
+    
+
+    public float bulletForce = 15f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+  public void Shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(FirePoint.forward * bulletForce, ForceMode.Impulse);
+    }
+
+
+
+}
