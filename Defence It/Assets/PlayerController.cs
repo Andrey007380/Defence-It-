5e68fc0e4ee6c9a7bbe3581673524cea01746542
Andrey007380 <Andrey.lishtva@gmail.com>
2021-03-11 21:30:13 +0200

    недоделанная системв стрельбы

--- Defence It/Assets/PlayerController.cs
+++ Defence It/Assets/PlayerController.cs
@@ -3,9 +3,14 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    public float moveSpeed = 0.5f;
+    public float moveSpeed = 100f;
+    public float angularSpeed = 0.5f;
+
     public float rotationX;
     public float rotationY;
+
+    public float transformX;
+    public float transformY;
 
     public Joystick joystick;
     private Rigidbody rigidbody;
@@ -13,18 +18,23 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rotationX = joystick.Horizontal * moveSpeed;
-        transform.Translate(rotationX * moveSpeed, 0, 0 );
+        transformX = joystick.Horizontal * moveSpeed;
+        transform.Translate(transformX * Time.deltaTime, 0, 0 );
 
-        rotationY = joystick.Vertical * moveSpeed;
-        transform.Translate(0, 0, rotationY * moveSpeed);
+/*        rotationX = joystick.Horizontal * angularSpeed;
+        transform.Rotate(0, rotationX, 0);*/
+
+        transformY = joystick.Vertical * moveSpeed;
+        transform.Translate(0, 0, transformY * Time.deltaTime);
+
+/*        rotationY = joystick.Horizontal * angularSpeed;
+        transform.Rotate(0, rotationY, 0);*/
 
     }
 }
